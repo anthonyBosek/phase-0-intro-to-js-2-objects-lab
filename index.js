d3b9fb4bac0cffe4773f5fbd4c1b238eaa994ae3
Anthony Bosek <abosek006@gmail.com>
2023-08-21 12:16:38 -0700

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = {
+  name: "dev_ant",
+  streetAddress: "100 Main St.",
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+  return { ...obj, [key]: value };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+  obj[key] = value;
+  return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+  const newEmployee = { ...obj };
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+  delete obj[key];
+  return obj;
+}
